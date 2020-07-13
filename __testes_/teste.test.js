89e4e5f20b503f0b5f4b174bc95f60232410c3f8
diegojimenes <contato.diegojimenes@gmail.com>
2020-07-13 09:08:30 -0300

    mock

--- __testes_/teste.test.js
+++ __testes_/teste.test.js
@@ -1,36 +1,37 @@
 import verificarNumeroDeTodosPendentes from '../src/todos'
 
-it('caminho feliz', () => {
-    const fetch = jest.fn(() => {
-        return new Promise((res) => {
-            let api = {
-                text: () => new Promise((res) => {
-                    let mockValue = JSON.stringify(
-                        [{
-                            "userId": 1,
-                            "id": 2,
-                            "title": "quis ut nam facilis et officia qui",
-                            "completed": false
-                        },
-                        {
-                            "userId": 1,
-                            "id": 3,
-                            "title": "fugiat veniam minus",
-                            "completed": false
-                        },
-                        {
-                            "userId": 1,
-                            "id": 4,
-                            "title": "et porro tempora",
-                            "completed": true
-                        }]
-                    )
-                    return res(mockValue)
-                })
-            }
-            return res(api)
-        })
+const fetch = jest.fn(() => {
+    return new Promise((res) => {
+        let api = {
+            text: () => new Promise((res) => {
+                let mockValue = JSON.stringify(
+                    [{
+                        "userId": 1,
+                        "id": 2,
+                        "title": "quis ut nam facilis et officia qui",
+                        "completed": false
+                    },
+                    {
+                        "userId": 1,
+                        "id": 3,
+                        "title": "fugiat veniam minus",
+                        "completed": false
+                    },
+                    {
+                        "userId": 1,
+                        "id": 4,
+                        "title": "et porro tempora",
+                        "completed": true
+                    }]
+                )
+                return res(mockValue)
+            })
+        }
+        return res(api)
     })
+})
+
+it('caminho feliz', () => {
     return verificarNumeroDeTodosPendentes(fetch, 1)
         .then((numeroDeTodosPendentes) => {
             expect(numeroDeTodosPendentes).toBe(2)
