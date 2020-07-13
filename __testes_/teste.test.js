498438fc3bf06f9e15e518baacbf33a18103af6a
diegojimenes <contato.diegojimenes@gmail.com>
2020-07-13 09:10:14 -0300

    rollback

--- __testes_/teste.test.js
+++ __testes_/teste.test.js
@@ -1,37 +1,36 @@
 import verificarNumeroDeTodosPendentes from '../src/todos'
 
-const fetch = jest.fn(() => {
-    return new Promise((res) => {
-        let api = {
-            text: () => new Promise((res) => {
-                let mockValue = JSON.stringify(
-                    [{
-                        "userId": 1,
-                        "id": 2,
-                        "title": "quis ut nam facilis et officia qui",
-                        "completed": false
-                    },
-                    {
-                        "userId": 1,
-                        "id": 3,
-                        "title": "fugiat veniam minus",
-                        "completed": false
-                    },
-                    {
-                        "userId": 1,
-                        "id": 4,
-                        "title": "et porro tempora",
-                        "completed": true
-                    }]
-                )
-                return res(mockValue)
-            })
-        }
-        return res(api)
-    })
-})
-
 it('caminho feliz', () => {
+    const fetch = jest.fn(() => {
+        return new Promise((res) => {
+            let api = {
+                text: () => new Promise((res) => {
+                    let mockValue = JSON.stringify(
+                        [{
+                            "userId": 1,
+                            "id": 2,
+                            "title": "quis ut nam facilis et officia qui",
+                            "completed": false
+                        },
+                        {
+                            "userId": 1,
+                            "id": 3,
+                            "title": "fugiat veniam minus",
+                            "completed": false
+                        },
+                        {
+                            "userId": 1,
+                            "id": 4,
+                            "title": "et porro tempora",
+                            "completed": true
+                        }]
+                    )
+                    return res(mockValue)
+                })
+            }
+            return res(api)
+        })
+    })
     return verificarNumeroDeTodosPendentes(fetch, 1)
         .then((numeroDeTodosPendentes) => {
             expect(numeroDeTodosPendentes).toBe(2)
